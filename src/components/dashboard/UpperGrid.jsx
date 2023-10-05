8cd47d8b65f3ed2b52e00df117d3872dc30fc7c5
KingShiv21 <shivrat503@gmail.com>
2023-10-05 15:39:06 +0530

    second commit

--- src/components/dashboard/UpperGrid.jsx
+++ src/components/dashboard/UpperGrid.jsx
@@ -5,8 +5,8 @@ const UpperGrid = () => {
   return (
     <div className=' px-1  w-full flex flex-col sm:flex-row justify-around'>
 
-      <div className=' bg-white w-full  my-1  sm:w-[17rem]'>
-        <div className=' flex flex-row items-center w-fit mx-auto gap-2'>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg'>
+        <div className='pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoBagHandle className=' bg-blue-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
 
@@ -21,8 +21,8 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem]'>
-        <div className='flex flex-row items-center w-fit mx-auto gap-2'>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg'>
+        <div className='pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoPieChart className=' bg-red-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
 
@@ -36,8 +36,8 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] '>
-        <div className='flex flex-row items-center w-fit mx-auto gap-2 '>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg '>
+        <div className=' pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2 '>
         <IoPeople className=' bg-yellow-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
 
@@ -51,8 +51,8 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] '>
-        <div className='flex flex-row items-center w-fit mx-auto gap-2'>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg '>
+        <div className=' pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoCart className=' bg-green-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
 
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -2,7 +2,6 @@ import { FcBullish } from "react-icons/fc"
 import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from "../lib/SidebarLinks"
 import { Link } from "react-router-dom"
 import { HiOutlineLogout } from "react-icons/hi"
-import { useState } from "react"
 
 
 const Sidebar = () => {
@@ -13,7 +12,7 @@ const Sidebar = () => {
 
     <div className=" w-8 h-full bg-neutral-900 sm:flex sm:flex-col sm:p-3 sm:w-60 sm:h-full sm:visible">
 
-      {logo <= 640 ? <FcBullish fontSize={24} /> :
+      {logo <= 640 ? <FcBullish fontSize={28} className=" mx-auto" /> :
 
 
         <div className=" flex flex-row px-1 py-3">
@@ -22,13 +21,21 @@ const Sidebar = () => {
         </div>
       }
 
-      <div className="  flex flex-col flex-1 pt-5">
+      <div className=" mt-10 sm:mt-1  flex flex-col flex-1 pt-5">
         {DASHBOARD_SIDEBAR_LINKS.map((items) => (
 
           <Link to={items.path} key={items.key} >
-            <div className=" flex flex-row my-1 py-1 rounded-md pl-2 hover:bg-neutral-700">
-              <span className=" text-xl text-blue-600">{items.icon}</span>
-              <span className=" invisible sm:visible text-sm text-blue-400 ml-1">{items.label}</span>
+            <div className=" flex flex-row my-1 py-1 rounded-md pl-1 sm:pl-2 hover:bg-neutral-700 ">
+              <span className=" text-2xl sm:text-xl text-blue-600">{items.icon}</span>
+
+              {logo <= 640 ? null :
+
+                <span className=" invisible sm:visible text-sm sm:text-blue-400 ml-1">{items.label}</span>
+
+              }
+
+
+
             </div>
           </Link>
 
@@ -36,21 +43,29 @@ const Sidebar = () => {
       </div>
 
       <hr className=" mt-20  mb-2 sm:mt-0" />
-      <div className="  flex flex-col">
+      <div className=" mt-10 sm:mt-0  flex flex-col">
         {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((items) => (
 
           <Link to={items.path} key={items.key} >
-            <div className=" flex flex-row my-1 py-1 rounded-md pl-2 hover:bg-neutral-700">
-              <span className=" text-xl text-blue-600">{items.icon}</span>
-              <span className=" invisible sm:visible text-sm text-blue-400 ml-1">{items.label}</span>
+            <div className=" flex flex-row my-1 py-1 rounded-md  pl-1 sm:pl-2 hover:bg-neutral-700">
+              <span className=" text-2xl sm:text-xl text-blue-600">{items.icon}</span>
+              {logo <= 640 ? null :
+
+                <span className=" invisible sm:visible text-sm text-blue-400 ml-1">{items.label}</span>
+
+              }
             </div>
           </Link>
 
         ))}
         <button>
-          <div className=" flex flex-row my-1 py-1 rounded-md pl-2 hover:bg-neutral-700">
-            <span className=" text-xl text-red-600"><HiOutlineLogout /></span>
-            <span className=" invisible sm:visible text-sm text-red-400 ml-1">Logout</span>
+          <div className=" flex flex-row my-1 py-1 rounded-md pl-1 sm:pl-2 hover:bg-neutral-700">
+            <span className=" text-2xl sm:text-xl text-red-600"><HiOutlineLogout /></span>
+            {logo <= 640 ? null :
+
+              <span className=" invisible sm:visible text-sm text-red-400 ml-1">Logout</span>
+
+            }
           </div>
         </button>
       </div>
