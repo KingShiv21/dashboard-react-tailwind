8872cfda8c76e846f2ecb1b42330976cf2801dd0
KingShiv21 <shivrat503@gmail.com>
2023-10-05 16:14:10 +0530

    third commit

--- src/components/dashboard/TransactionChart.jsx
+++ src/components/dashboard/TransactionChart.jsx
@@ -64,27 +64,29 @@ const data = [
 ]
 const TransactionChart = () => {
     return (
-        <div className=' h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1'>
+        <div className=' h-[22rem] bg-white py-1 sm:p-4 rounded-sm border border-gray-200 flex flex-col flex-1'>
             <strong className="text-gray-700 font-medium text-center tece">Transactions</strong>
-            <div className="mt-3 w-full flex-1 text-xs">
+            <div className="mt-3 w-full h-[16rem] sm:h-[20rem] text-[8px] sm:text-xs">
 
                 <ResponsiveContainer width="100%" height="100%">
                     <BarChart
                         data={data}
-                        margin={{
-                            top: 20,
-                            right: 10,
-                            left: -10,
-                            bottom: 0
-                        }}>
+                        margin={
+                            {
+                                right: 20,
+                                left: -20
+                            }
+                        }
 
-<CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Bar dataKey="Income" fill="#0ea5e9" />
-						<Bar dataKey="Expense" fill="#ea580c" />
+                    >
+
+                        <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="Income" fill="#0ea5e9" />
+                        <Bar dataKey="Expense" fill="#ea580c" />
 
                     </BarChart>
                 </ResponsiveContainer>
--- src/components/dashboard/UpperGrid.jsx
+++ src/components/dashboard/UpperGrid.jsx
@@ -5,7 +5,7 @@ const UpperGrid = () => {
   return (
     <div className=' px-1  w-full flex flex-col sm:flex-row justify-around'>
 
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg'>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg sm:rounded-none'>
         <div className='pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoBagHandle className=' bg-blue-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
@@ -21,7 +21,7 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg'>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg sm:rounded-none'>
         <div className='pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoPieChart className=' bg-red-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
@@ -36,7 +36,7 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg '>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg sm:rounded-none'>
         <div className=' pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2 '>
         <IoPeople className=' bg-yellow-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
@@ -51,7 +51,7 @@ const UpperGrid = () => {
         </div>
 
       </div>
-      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg '>
+      <div className=' bg-white w-full  my-1  sm:w-[17rem] rounded-lg sm:rounded-none '>
         <div className=' pl-2 sm:pl-0  flex flex-row items-center w-fit sm:mx-auto gap-2'>
         <IoCart className=' bg-green-600 rounded-full text-white w-9 h-9 p-2'/>
         <div className=' flex flex-col'>
